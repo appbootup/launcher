82e2dc8af82c1da4206856d084d0ba9ed416c370
mrchainhead <mrchainhead@gmail.com>
2023-04-15 18:01:25 +0530

    Initialisation is through SASL authentication.

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -4,28 +4,33 @@ kafka-topics \
   --create --if-not-exists \
   --topic ${MACHINE_ID}_data   \
   --replication-factor ${REPLICATION_FACTOR} \
-  --partitions ${PARTITION_COUNT}
+  --partitions ${PARTITION_COUNT} \
+  --command-config ${CONFIG_PROPS}
 kafka-topics \
   --bootstrap-server ${BROKER_DNS}:${BROKER_PORT} \
   --create --if-not-exists \
   --topic ${MACHINE_ID}_limits   \
   --replication-factor ${REPLICATION_FACTOR} \
-  --partitions ${PARTITION_COUNT}
+  --partitions ${PARTITION_COUNT} \
+  --command-config ${CONFIG_PROPS}  
 kafka-topics \
   --bootstrap-server ${BROKER_DNS}:${BROKER_PORT} \
   --create --if-not-exists \
   --topic ${MACHINE_ID}_alarms   \
   --replication-factor ${REPLICATION_FACTOR} \
-  --partitions ${PARTITION_COUNT}  
+  --partitions ${PARTITION_COUNT} \
+  --command-config ${CONFIG_PROPS}  
 kafka-topics \
   --bootstrap-server ${BROKER_DNS}:${BROKER_PORT} \
   --create --if-not-exists \
   --topic ${MACHINE_ID}_alerts   \
   --replication-factor ${REPLICATION_FACTOR} \
-  --partitions ${PARTITION_COUNT}
+  --partitions ${PARTITION_COUNT} \
+  --command-config ${CONFIG_PROPS}  
 kafka-topics \
   --bootstrap-server ${BROKER_DNS}:${BROKER_PORT} \
   --create --if-not-exists \
   --topic ${MACHINE_ID}_events   \
   --replication-factor ${REPLICATION_FACTOR} \
-  --partitions ${PARTITION_COUNT}
+  --partitions ${PARTITION_COUNT} \
+  --command-config ${CONFIG_PROPS}  
